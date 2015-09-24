86f202e8dc9be55255fe121cc96a3b5d2bf2a781
Perry Wang <perrywangsh@gmail.com>
2015-09-24 15:50:39 +0800

    add customization capability

--- js/fileuploader.jquery.js
+++ js/fileuploader.jquery.js
@@ -1,77 +1,72 @@
 (function($){
-	
-    var FileUploader = function(options){
-        this.$el = options.container;
-    	this.url = options.url;
-    	this.render = function(){
-    		var that = this;
-    		this.$el.append('<div class="ui grid">\
-								  <div class="row">\
-								    <div class="three wide column">\
-								      <div id="attaching" class="ui teal fluid left labeled icon button">Attaching Files&nbsp;&nbsp;....<i class="attach icon"></i></div>\
-								    </div>\
-								    <div class="three wide column">\
-								      <div id="upload" class="ui teal fluid left labeled icon button">Upload<i class="cloud upload icon"></i></div>\
-								    </div>\
-								  </div>\
-								  <div class="row">\
-								    <div class="six wide column">\
-								      <form action="/upload" method="post" enctype="multipart/form-data"></form>\
-								    </div>\
-								  </div>\
-                                  <div class="row">\
-                                    <div class="three wide column">\
-                                       <h4>File Name</h4>\
-                                    </div>\
-                                    <div class="right aligned three wide column">\
-                                       <h4>Size</h4>\
-                                    </div>\
-                                  </div>\
-                                  <div class="row" style="margin-top:-30px">\
-                                    <div class="six wide column">\
-                                        <div class="ui divider">\
-                                        </div>\
-                                    </div>\
-                                  </div>\
-								  <div class="row" style="margin-top:-30px">\
-								    <div class="six wide column">\
-								      <div class="ui divided list"></div>\
-								    </div>\
-								  </div>\
-								  <div class="row">\
-								    <div class="six wide column">\
-								      <div id="progress" style="margin-top:20px;" data-percent="0" class="ui teal large progress">\
-								        <div class="bar"></div>\
-								        <div class="label">uploading files</div>\
-								      </div>\
-								    </div>\
-								  </div>\
-								</div>'
-							);
-
+	var defaultOptions = {
+        template:'<div class="ui grid">\
+                      <div class="row">\
+                        <div class="three wide column">\
+                          <div id="attaching" class="ui teal fluid left labeled icon button">Attaching Files&nbsp;&nbsp;....<i class="attach icon"></i></div>\
+                        </div>\
+                        <div class="three wide column">\
+                          <div id="upload" class="ui teal fluid left labeled icon button">Upload<i class="cloud upload icon"></i></div>\
+                        </div>\
+                      </div>\
+                      <div class="row">\
+                        <div class="six wide column">\
+                          <form action="/upload" method="post" enctype="multipart/form-data"></form>\
+                        </div>\
+                      </div>\
+                      <div class="row">\
+                        <div class="three wide column">\
+                           <h4>File Name</h4>\
+                        </div>\
+                        <div class="right aligned three wide column">\
+                           <h4>Size</h4>\
+                        </div>\
+                      </div>\
+                      <div class="row" style="margin-top:-30px">\
+                        <div class="six wide column">\
+                            <div class="ui divider">\
+                            </div>\
+                        </div>\
+                      </div>\
+                      <div class="row" style="margin-top:-30px">\
+                        <div class="six wide column">\
+                          <div class="ui divided list"></div>\
+                        </div>\
+                      </div>\
+                      <div class="row">\
+                        <div class="six wide column">\
+                          <div id="progress" style="margin-top:20px;" data-percent="0" class="ui teal large progress">\
+                            <div class="bar"></div>\
+                            <div class="label">uploading files</div>\
+                          </div>\
+                        </div>\
+                      </div>\
+                    </div>',
+        itemTempalte:'<div class="item">\
+                          <div class="content">\
+                            <div class="ui grid">\
+                              <div style="margin-top:10px;margin-bottom:10px;" class="ui two column row">\
+                                <div class="column"><a>{{name}}</a></div>\
+                                <div class="right aligned column"><a>{{size}}</a></div>\
+                              </div>\
+                            </div>\
+                          </div>\
+                      </div>',
+        render:function(){
+            var that = this;
+            this.$el.append(this.template);
             var form = this.$el.find('form');
             var attachingBtn = this.$el.find('#attaching');
             var uploadBtn = this.$el.find('#upload');
             var fileList = this.$el.find('.ui.list');
-			var progressBar = this.$el.find('#progress');
-			var progressBarLabel = this.$el.find('#progress .label');
+            var progressBar = this.$el.find('#progress');
+            var progressBarLabel = this.$el.find('#progress .label');
             progressBar.hide();
-			attachingBtn.on('click',function(event){
-				form.append('<input type="file" multiple="multiple" name="files[]" style="display:none;">');
+            attachingBtn.on('click',function(event){
+                form.append('<input type="file" multiple="multiple" name="files[]" style="display:none;">');
                 var clickingInput = form.find('input:last');
                 clickingInput.on('change', function () {
-
-                    var itemTempalte = '<div class="item">\
-										  <div class="content">\
-										    <div class="ui grid">\
-										      <div style="margin-top:10px;margin-bottom:10px;" class="ui two column row">\
-										        <div class="column"><a>{{name}}</a></div>\
-										        <div class="right aligned column"><a>{{size}}</a></div>\
-										      </div>\
-										    </div>\
-										  </div>\
-										</div>';
-                    
+                    var itemTempalte = that.itemTempalte;                  
                     var itemTemplateRender = Handlebars.compile(itemTempalte);
                     var fileNumber = this.files.length;
                     that.total += fileNumber;
@@ -82,7 +77,7 @@
 
                 });
                 clickingInput.click();
-			});
+            });
             
             uploadBtn.on('click',function(event){
                 progressBar.removeClass('error success');    
@@ -93,13 +88,13 @@
                 $(this).toggleClass("disabled");
 
                 var uploadingCallback = function (e) {
-	                if (e.lengthComputable) {
-	                    var max = e.total;
-	                    var current = e.loaded;
-	                    var Percentage = Math.round((current * 100) / max); 
-	                    progressBar.progress({percent:Percentage});
-	                    progressBarLabel.text(''+ Percentage + ' % uploaded');              
-	                }
+                    if (e.lengthComputable) {
+                        var max = e.total;
+                        var current = e.loaded;
+                        var Percentage = Math.round((current * 100) / max); 
+                        progressBar.progress({percent:Percentage});
+                        progressBarLabel.text(''+ Percentage + ' % uploaded');              
+                    }
                 };
 
                 var formData = new FormData(form[0]);
@@ -129,10 +124,15 @@
                         uploadBtn.removeClass('disabled');
 
                 });            
-            });
-
-			
-    	}
+            });           
+        }
+    };
+    var FileUploader = function(options){
+        this.$el = options.container;
+    	this.url = options.url;
+        this.template = options.template || defaultOptions.template;
+        this.itemTempalte = options.itemTempalte || defaultOptions.itemTempalte;
+    	this.render = options.render || defaultOptions.render;
     };
 
 	$.fn.fileUploader = function(options){
